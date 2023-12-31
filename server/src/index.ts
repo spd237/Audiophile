b3990ae95be3aa7a37eb4dbf75ad1c9a47701db0
tonishehaj12@gmail.com <89846672+ToniSh69@users.noreply.github.com>
2023-12-31 12:59:05 +0100

    minor fixes

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,14 @@
 import express from 'express';
 import router from './router';
 import cors from 'cors';
+import helmet from 'helmet';
 import { handleSignUp } from './handlers/auth/handleSignUp';
 import { handleSignIn } from './handlers/auth/handleSignIn';
 
 const app = express();
 const PORT = 3000;
 app.use(cors());
+app.use(helmet());
 app.use(express.json());
 
 app.use('/audiophile', router);
